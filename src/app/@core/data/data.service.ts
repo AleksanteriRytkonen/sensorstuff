f94315a69d7420ff6b01af0163e8dce5c98809d6
Aleksanteri Rytkönen <alrytkonen@gmail.com>
2018-07-31 08:12:27 +0300

    add general error catcher on dataService

--- src/app/@core/data/data.service.ts
+++ src/app/@core/data/data.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { tap, map } from 'rxjs/operators';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { tap, map, catchError } from 'rxjs/operators';
 import { SensorData } from '../models/sensordata';
-import { Observable } from 'rxjs';
+import { Observable, throwError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +12,7 @@ export class DataService {
   apiUrl = 'http://192.168.1.106:5000';
   tempsUrl = '/sensors/';
 
-  httpOptions = {
+  headers = {
     headers: new HttpHeaders({
       'Content-Type': 'application/json'
     })
@@ -21,10 +21,26 @@ export class DataService {
   constructor(private http: HttpClient) { }
 
   getTemps(): Observable<SensorData[]> {
-    return this.http.get<SensorData[]>(this.apiUrl + this.tempsUrl, this.httpOptions).pipe(
-      /* tap(data => console.log(data)) */
+    return this.http.get<SensorData[]>(this.apiUrl + this.tempsUrl, this.headers).pipe(
+      catchError(this.handleError)
     );
   }
+
+  private handleError(error: HttpErrorResponse) {
+    if (error.error instanceof ErrorEvent) {
+      // A client-side or network error occurred. Handle it accordingly.
+      console.error('An error occurred:', error.error.message);
+    } else {
+      // The backend returned an unsuccessful response code.
+      // The response body may contain clues as to what went wrong,
+      console.error(
+        `Backend returned code ${error.status}, ` +
+        `body was: ${error.error}`);
+    }
+    // return an observable with a user-facing error message
+    return throwError(
+      'Something bad happened; please try again later.');
+  };
 
 
 }
